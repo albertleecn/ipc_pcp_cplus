7e7922b204993bbdb40f1dffcb0f65ffbd2b5b4f
albert <albert_lee@163.com>
2015-04-18 10:40:23 +0800

    Initial Commit

---
+++ ipc_pcp_cplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ipc_pcp_cplus
+//
+//  Created by albert on 15/4/18.
+//  Copyright (c) 2015年 albert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
